5310c062f4a40b7cf82f3c579e6c3da4f7e29f38
AanviKothari456 <73425604+AanviKothari456@users.noreply.github.com>
2021-01-19 16:24:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,6 +10,8 @@ var eduGroup, pollGroup,pandGroup, unempGroup, gwGroup;
 var poll,unemp,edu;
 var score=0;
 
+var yes, no;
+
 var gameOver, restart, bg;
 
 function preload(){
@@ -40,7 +42,7 @@ function setup() {
   bg.scale=3.5;
   bg.velocityY=-2;
   
-  restart = createSprite(300,140);
+  restart = createSprite(500,340);
   restart.addImage(restartImg);
   restart.scale = 0.5;
   restart.visible = false;
@@ -51,14 +53,22 @@ function setup() {
 
   
 
-  man= createSprite(1000,600);
+  man= createSprite(900,600);
   //man.velocityY=2;
   man.addAnimation("running", man_running);
   man.scale = 0.6;
 
-  problem = createSprite(man.x,50);
+  problem = createSprite(man.x,30);
 
- 
+  yes=createSprite(700,400,50,50);
+  yes.addImage(yesImg);
+  yes.scale =0.2;
+  yes.visible = false;
+  no=createSprite(1200,400,50,50);
+  no.addImage(noImg);
+  no.scale = 0.2;
+  no.visible = false;
+
   eduGroup = new Group();
   unempGroup = new Group();
   pollGroup = new Group();
@@ -77,30 +87,34 @@ function draw() {
     switch(gameState){
     case "level1": problem.addImage(eduImg);
                    problem.scale =0.5;
-                   problem.velocityY = 1;
+                   problem.velocityY = 0.1;
                    bg.velocityY=-2;
                   EduCollection();
                   break;
-    case "level2": problem.addImage(umempImg);
-                  problem.velocityY = 1.5;
+    case "level2":
+                  problem.addImage(umempImg);
+                  problem.velocityY = 0.2;
                   problem.scale =0.5;
                   bg.velocityY=-3;
                   unempCollection();
                   break;   
-    case "level3": problem.addImage(pollImg);
-                  problem.velocityY = 2;
+    case "level3": 
+                  problem.addImage(pollImg);
+                  problem.velocityY = 0.3;
                   problem.scale =0.5;
                   bg.velocityY=-4;
                   pollCollection();
                   break;          
-    case "level4": problem.addImage(pandImg);
-                  problem.velocityY = 2.5;
+    case "level4": 
+                  problem.addImage(pandImg);
+                  problem.velocityY = 0.4;
                   problem.scale =0.5;
                   bg.velocityY=-5;
                   pandCollection();
                   break; 
-    case "level5": problem.addImage(gwImg);
-                  problem.velocityY = 3;
+    case "level5": 
+                  problem.addImage(gwImg);
+                  problem.velocityY = 0.5;
                   problem.scale =0.5;
                   bg.velocityY=-5;
                   gwCollection();
@@ -144,9 +158,9 @@ function draw() {
     unempGroup.destroyEach();
    score=score+1;
   }
-  /*if (problem.isTouching(man)){
-    gameState = "end";
-  }*/
+ 
+ 
+
   drawSprites();
   fill('pink');
     rect(479,30,120,40);
@@ -157,22 +171,27 @@ function draw() {
   if(score === 5){
     gameState2 = 1;
   Questions();
+   }
+   }
+   if (problem.isTouching(man)){
+    gameState = "end";
   }
-  }
+
   if(gameState ==="end"){ 
     background(0);
     fill('red');
-    text('TRY AGAIN TO SAVE THE EARTH', 500,500);
+    text('GO AGAIN TO SAVE THE EARTH', 500,500);
     restart.visible = true;
   console.log("end");
   if(mousePressedOver(restart)) {
     reset();
   }
+  drawSprites();
  }
 }
 function EduCollection(){
   if (frameCount%50===0 && gameState=="level1"&& gameState2 === 0){
-    var pencil = createSprite(random(400, windowWidth-400), random(problem.y, windowHeight), 100, 100);
+    var pencil = createSprite(random(400, windowWidth-600), random(problem.y, windowHeight), 100, 100);
     pencil.velocityX = 6;
     pencil.lifetime=500;
     pencil.addImage(pencilImg);
@@ -187,7 +206,7 @@ function unempCollection(){
       job.velocityX = 6;
       job.lifetime=500;
       job.addImage(jobImg);
-      job.scale =0.1;
+      job.scale =0.2;
       unempGroup.add(job);
       }
       
@@ -198,7 +217,7 @@ function pollCollection(){
        tree.velocityX = 6;
        tree.lifetime=500;
        tree.addImage(treeImg);
-       tree.scale =0.1;
+       tree.scale =0.2;
        pollGroup.add(tree);
        }
         
@@ -210,7 +229,7 @@ function pandCollection(){
           vaccine.velocityX = 6;
           vaccine.lifetime=500;
           vaccine.addImage(vaccineImg);
-          vaccine.scale =0.1;
+          vaccine.scale =0.5;
           pandGroup.add(vaccine);
          }
           
@@ -229,97 +248,109 @@ function gwCollection(){
         
    function Questions(){
      
-    var yes=createSprite(700,400,50,50);
-    yes.addImage(yesImg);
-    yes.scale =0.2;
-    var no=createSprite(1200,400,50,50);
-    no.addImage(noImg);
-    no.scale = 0.2;
-
-    //Change Questions according to level, change gameState according to levels. 
+    console.log(yes);
+                        
+       //Change Questions according to level, change gameState according to levels. 
     switch(gameState){
       case "level1":    
-                         textSize(25);
-                        text(' Do you think that inequalities like gender and cultural identity should affect education’s reach?', 400,250);
-                      if(mousePressedOver(yes)){
-                        no.destroy();
-                        yes.destroy()
-                        gameState = "end";
+                         textSize(40);
+                        text(' Do you think that inequalities like gender and cultural identity should affect education’s reach?', 50,250);
+                        yes.visible = true;
+                        no.visible = true;
+                        if(mousePressedOver(yes)){
+                          yes.visible = false;
+                          no.visible = false;
+                        
+                         gameState = "end";
                       }else if(mousePressedOver(no)){
-                        no.destroy();
-                        yes.destroy();
-                        gameState = "level2";
+                        yes.visible = false;
+                        no.visible = false;
+                                            
+                       gameState = "level2";
                         gameState2 = 0;
                         score = 0;
-                      }
-                      console.log(gameState);
+                        }
+                      
                     break;
-      case "level2": text('If you met a stranger on the road who is looking for a job, would you reference him/her to a potential employee you know of?', 400,400);
+      case "level2": 
+      textSize(30);
+      text('If you met a stranger on the road who is looking for a job, would you reference him/her to a potential employee you know of?', 50,250);
+      yes.visible = true;
+      no.visible = true;
       if(mousePressedOver(no)){
-        no.destroy();
-        yes.destroy()
+        yes.visible = false;
+        no.visible = false;
         gameState = "end";
       }else if(mousePressedOver(yes)){
-        no.destroy();
-        yes.destroy();
+        yes.visible = false;
+        no.visible = false;
         gameState = "level3";
         gameState2 = 0;
         score = 0;
       }
-      console.log(gameState);
+     
     break;
-      case "level3": text(' Do you believe in reusing materials like newspaper and other scraps and wish to spread awareness about pollution?', 400,400);
+      case "level3": 
+      textSize(30);
+      text(' Do you believe in reusing materials like newspaper and other scraps and wish to spread awareness about pollution?', 50,250);
+      yes.visible = true;
+      no.visible = true;
       if(mousePressedOver(no)){
-        no.destroy();
-        yes.destroy()
+        yes.visible = false;
+        no.visible = false;
         gameState = "end";
       }else if(mousePressedOver(yes)){
-        no.destroy();
-        yes.destroy();
+        yes.visible = false;
+        no.visible = false;
         gameState = "level4";
         gameState2 = 0;
         score = 0;
       }
-      console.log(gameState);
+      
     break;
-      case "level4": text("Do you think that you should obey sanitation rules during a pandemic if others aren't? ", 400,400);
+      case "level4": textSize(30);
+      text("Do you think that you should obey sanitation rules during a pandemic if others aren't? ", 50,250);
+      yes.visible = true;
+      no.visible = true;
       if(mousePressedOver(no)){
-        no.destroy();
-        yes.destroy()
+        yes.visible = false;
+       no.visible = false;
         gameState = "end";
       }else if(mousePressedOver(yes)){
-        no.destroy();
-        yes.destroy();
+        yes.visible = false;
+        no.visible = false;
         gameState = "level5";
         gameState2 = 0;
         score = 0;
       }
-      console.log(gameState);
+     
     break;
-      case "level5": text(' Do you think switching off your lights and fans and reducing water waste can prevent pollution?', 400,400);
+      case "level5": textSize(30);
+      text(' Do you think switching off your lights and fans and reducing water waste can prevent pollution?', 50,250);
+      yes.visible = true;
+      no.visible = true;
       if(mousePressedOver(yes)){
-        no.destroy();
-        yes.destroy()
+        yes.visible = false;
+       no.visible = false;
         gameState = "end";
       }else if(mousePressedOver(no)){
-        no.destroy();
-        yes.destroy()
+        yes.visible = false;
+       no.visible = false;
         gameState = "end";
       }
-      console.log(gameState);
+   
     break;
       }
-
+                
     }
   
     function reset(){
       gameState = "level1";
-    
       restart.visible = false;
-      
+      yes.visible = false;
+      no.visible = false;
       EduCollection.destroyEach();
      
-      
       score = 0;
       
     }
